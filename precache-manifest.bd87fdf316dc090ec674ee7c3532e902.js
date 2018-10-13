2f68fc7c6a22d0d4083460e8495e686b7affb5ce
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-13 13:10:34 +0200

    Updates

---
+++ precache-manifest.bd87fdf316dc090ec674ee7c3532e902.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "096a59c7e4139ea6908d",
+    "url": "/react-apexcharts/static/js/runtime~main.096a59c7.js"
+  },
+  {
+    "revision": "54d431ec8a4ec890dac0",
+    "url": "/react-apexcharts/static/js/main.54d431ec.chunk.js"
+  },
+  {
+    "revision": "b1446b6c60a7b677fdfd",
+    "url": "/react-apexcharts/static/js/1.b1446b6c.chunk.js"
+  },
+  {
+    "revision": "7f2b946fb7cc90c09b66aec15418bd50",
+    "url": "/react-apexcharts/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.720e5d971e210c35a1ed8b7938b21a1f.js"
+  "/react-apexcharts/precache-manifest.bd87fdf316dc090ec674ee7c3532e902.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-apexcharts/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
