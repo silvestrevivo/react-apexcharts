b6e338af203be74687442e19a40d5c3e19dde09d
silvestrevivo <silvestrevivomillan@gmail.com>
2018-10-13 12:59:50 +0200

    Updates

---
+++ precache-manifest.720e5d971e210c35a1ed8b7938b21a1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54d431ec8a4ec890dac0",
+    "url": "/static/js/main.54d431ec.chunk.js"
+  },
+  {
+    "revision": "b1446b6c60a7b677fdfd",
+    "url": "/static/js/1.b1446b6c.chunk.js"
+  },
+  {
+    "revision": "f82259e4def2d617b494f35bc74b525e",
+    "url": "/index.html"
+  }
+];
